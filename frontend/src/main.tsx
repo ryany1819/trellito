bfd022c9f25c2576b7e5ba9fc20f88f0141387be
Ryan Yang <ryany1819@gmail.com>
2025-05-31 11:42:42 -0700

    feat: add ThemeProvider component and integrate it into the main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+import { ThemeProvider } from "@/components/theme-provider";
 
-if (import.meta.env.DEV && import.meta.env.VITE_USE_MSW === 'true') {
-  console.log('Running in dev:mock mode');
-  const { worker } = await import('./mocks/browser.ts');
+if (import.meta.env.DEV && import.meta.env.VITE_USE_MSW === "true") {
+  console.log("Running in dev:mock mode");
+  const { worker } = await import("./mocks/browser.ts");
   await worker.start();
-  console.log('Mock service worker started');
+  console.log("Mock service worker started");
 }
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+    <ReduxProvider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ReduxProvider>
+  </StrictMode>
+);
